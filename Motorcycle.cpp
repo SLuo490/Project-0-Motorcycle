05caf733531c3a86a895895f2307d4028c5a9bc2
Shi Tao Luo <sluo5991@gmail.com>
2021-02-08 08:34:16 -0500

    fixed edge cases for parameterized constructor

--- Motorcycle.cpp
+++ Motorcycle.cpp
@@ -4,24 +4,28 @@
 //default constructor
 Motorcycle::Motorcycle()
 {
+  brand_ = bike_details::bike_brand(rand() % 4);
   curr_speed_ = 0;
   curr_acceleration_ = bike_details::NONE;
   curr_direction_ = 0;
   distance_traveled_ = 0;
-  brand_ = bike_details::bike_brand(rand() % 4);
 }
 
 //parameterized constructor
 Motorcycle::Motorcycle(int kind_of_bike)
 {
-  if ((kind_of_bike < 0) && (kind_of_bike > 4))
+  if ((kind_of_bike < 0) || (kind_of_bike > 3))
   {
-    std::cout << "Enter a new number between 0 - 4" << std::endl;
+    throw std::runtime_error("Error: Enter a bike type number between 0 and 3");
   }
   else
   {
     brand_ = bike_details::bike_brand(kind_of_bike);
   }
+  curr_speed_ = 0;
+  curr_acceleration_ = bike_details::NONE;
+  curr_direction_ = 0;
+  distance_traveled_ = 0;
 }
 
 //Direction
@@ -136,21 +140,32 @@ void Motorcycle::updateSpeed()
   curr_speed_ = (curr_acceleration_ / 8) + (brand_ * 17.64);
 }
 
-// void Motorcycle::set_Direction (int direction) {
-//   curr_direction_ = direction;
-// }
-
-// void
-
 int main()
 {
+  //default constructor
+  //Not Random Need Fix
   Motorcycle test;
-  test.turn(0); //northwest
-  //current direction
-  std::cout << "Current Direction: " << test.getDirection() << std::endl;
 
   //bike type
-  std::cout << "Current Bike Type: " << test.getBikeType() << std::endl;
+  std::cout << "Random Bike Type: " << test.getBikeType() << std::endl;
+
+  //This catches the edge cases: less than 0 and greater than 3
+  try
+  {
+    Motorcycle test2(0);
+    std::cout << "Assigned Bike Type: " << test2.getBikeType() << std::endl;
+  }
+  catch (std::exception const &e)
+  {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  //East
+  test.turn(0);
+
+  //current direction
+  std::cout << "Current Direction: " << test.getDirection() << std::endl;
 
   return 0;
 }
