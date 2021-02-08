98e1a30d54b2475a0ab1d81aa76642b10abbd471
Shi Tao Luo <sluo5991@gmail.com>
2021-02-08 10:23:12 -0500

    fixed compile error

---
+++ main.cpp
@@ -0,0 +1,36 @@
+/**
+ Name: Shi Tao Luo
+ Date: Deb 8, 2021
+ Program: Project 0
+ */
+#include <iostream>
+#include "Motorcycle.hpp"
+
+int main()
+{
+  //default constructor
+  Motorcycle test;
+
+  //bike type
+  std::cout << "Random Bike Type: " << test.getBikeType() << std::endl;
+
+  //This catches the edge cases: less than 0 and greater than 3
+  try
+  {
+    Motorcycle test2(0);
+    std::cout << "Assigned Bike Type: " << test2.getBikeType() << std::endl;
+  }
+  catch (std::exception const &e)
+  {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  //Test East
+  test.turn(0);
+
+  //Get current direction
+  std::cout << "Current Direction: " << test.getDirection() << std::endl;
+
+  return 0;
+}
